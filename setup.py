68ef3f5703b84b1a4d0b68fc05f78c8a1f7e371b
merovingien <pg.ensiie@gmail.com>
2013-01-23 07:01:17 +0100

    Update setup.py to generate aiwar python module

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-cxxsrc = ["config.cpp", "item.cpp", "living.cpp", "movable.cpp", "playable.cpp", "memory.cpp", "mineral.cpp", "base.cpp", "miningship.cpp", "fighter.cpp", "missile.cpp", "item_manager.cpp", "game_manager.cpp", "python_wrapper.cpp"]
+cxxsrc = ["config.cpp", "item.cpp", "living.cpp", "movable.cpp", "playable.cpp", "memory.cpp", "mineral.cpp", "base.cpp", "miningship.cpp", "fighter.cpp", "missile.cpp", "item_manager.cpp", "game_manager.cpp", "stat_manager.cpp", "python_wrapper.cpp"]
 
 
 setup(name="aiwar", version="1.0-beta1",
